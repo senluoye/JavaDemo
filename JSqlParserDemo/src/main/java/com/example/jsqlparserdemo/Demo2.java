3cc00080e3469bdb1049a201f722bec4dc209ebb
senluoye <57495136+senluoye@users.noreply.github.com>
2022-12-06 17:31:12 +0800

    jsqpparser

---
+++ JSqlParserDemo/src/main/java/com/example/jsqlparserdemo/Demo2.java
@@ -0,0 +1,8 @@
+package com.example.jsqlparserdemo;/**
+ * @ClassName Demo2
+ * @Description 
+ * @Author QKS
+ * @Version v1.0
+ * @Create 2022-12-05 17:21
+ */public class Demo2 {
+}
---
+++ JSqlParserDemo/src/main/java/com/example/jsqlparserdemo/buildSqlDemo.java
@@ -0,0 +1,8 @@
+package com.example.jsqlparserdemo;/**
+ * @ClassName buildSqlDemo
+ * @Description 
+ * @Author QKS
+ * @Version v1.0
+ * @Create 2022-12-05 16:58
+ */public class buildSqlDemo {
+}
