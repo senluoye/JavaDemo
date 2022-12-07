460d055663c311e8680b0903d35dfe4130e25a54
senluoye <57495136+senluoye@users.noreply.github.com>
2022-12-07 10:37:29 +0800

    坐牢

--- JSqlParserDemo/src/main/java/com/example/jsqlparserdemo/Demo2.java
+++ JSqlParserDemo/src/main/java/com/example/jsqlparserdemo/Demo2.java
@@ -1,8 +1,59 @@
-package com.example.jsqlparserdemo;/**
+package com.example.jsqlparserdemo;
+
+import net.sf.jsqlparser.parser.*;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.Statement;
+import org.junit.Test;
+
+/**
  * @ClassName Demo2
- * @Description 
+ * @Description 实现为字段名和表明加上双引号
  * @Author QKS
  * @Version v1.0
  * @Create 2022-12-05 17:21
- */public class Demo2 {
+ */
+public class Demo2 {
+    @Test
+    public void test10ParseVisitor() throws ParseException {
+        String sql = "SELECT person.id,person.name,group.name FROM person JOIN group ON person.group_id = group.id WHERE person.birthdat > '1980-01-01'";
+        /** 创建SQL语句解析器实例 */
+        CCJSqlParser parser = CCJSqlParserUtil.newParser(sql);
+        /** 解析SQL语句 */
+        Statement stmt = parser.Statement();
+        /** 使用 LogVisiter对象遍历AST的所有节点 */
+        parser.getASTRoot().jjtAccept(new LogVisitor(), null);
+
+        System.out.println(stmt);
+    }
+
+    /**
+     * 遍历所有节点的{@link net.sf.jsqlparser.parser.CCJSqlParserVisitor} 接口实现类
+     *
+     * @author guyadong
+     */
+    static class LogVisitor extends CCJSqlParserDefaultVisitor {
+        @Override
+        public Object visit(SimpleNode node, Object data) {
+            Object value = node.jjtGetValue();
+            /** 根据节点类型找出表名和字段名节点，对名字加上双引号 */
+            if (node.getId() == CCJSqlParserTreeConstants.JJTCOLUMN) {
+                System.out.println(value.toString());
+                Column column = (Column) value;
+                column.setColumnName("\"" + column.getColumnName() + "\"");
+                Table table = column.getTable();
+                if (null != table) {
+                    table.setName("\"" + table.getName() + "\"");
+                }
+            } else if (node.getId() == CCJSqlParserTreeConstants.JJTTABLE) {
+                System.out.println(value.toString());
+                Table table = (Table) value;
+                table.setName("\"" + table.getName() + "\"");
+            } else if (null != value) {
+                /** 其他类型节点输出节点类型值，Java类型和节点值 */
+                System.out.println(node.getId() + ", " + value.getClass().getSimpleName() + ", " + value);
+            }
+            return super.visit(node, data);
+        }
+    }
 }
--- JSqlParserDemo/src/main/java/com/example/jsqlparserdemo/buildSqlDemo.java
+++ JSqlParserDemo/src/main/java/com/example/jsqlparserdemo/buildSqlDemo.java
@@ -1,8 +1,114 @@
-package com.example.jsqlparserdemo;/**
+package com.example.jsqlparserdemo;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.delete.Delete;
+import net.sf.jsqlparser.statement.insert.Insert;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.update.Update;
+import net.sf.jsqlparser.util.SelectUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
  * @ClassName buildSqlDemo
- * @Description 
+ * @Description jsqlparser 可以通过java代码构建sql
  * @Author QKS
  * @Version v1.0
  * @Create 2022-12-05 16:58
- */public class buildSqlDemo {
+ */
+public class buildSqlDemo {
+    /**
+     * 简单的构建单表查询
+     *
+     * @throws JSQLParserException
+     */
+    public static void main(String[] args) throws JSQLParserException {
+        Select select01 = SelectUtils.buildSelectFromTable(new Table("test"));
+        System.err.println(select01.getSelectBody().toString()); // SELECT * FROM test
+        Select select02 = SelectUtils.buildSelectFromTableAndExpressions(new Table("test"), new Column("col1"), new Column("col2"));
+        System.err.println(select02.getSelectBody().toString()); // SELECT col1, col2 FROM test
+        Select select03 = SelectUtils.buildSelectFromTableAndExpressions(new Table("mytable"), "col1", "col2");
+        System.err.println(select03.getSelectBody().toString()); // SELECT col1, col2 FROM test
+    }
+
+    /**
+     * 构建插入语句
+     */
+    @Test
+    public void buildInsertSql() {
+        // 创建表对象设置表名
+        Table table = new Table();
+        table.setName("user");
+
+        // 创建插入对象
+        Insert insert = new Insert();
+        insert.setTable(table); // 设置插入对象的表对象
+
+        // 设置插入列
+        List<Column> columnList = Arrays.asList(new Column("col01"), new Column("col02"));
+        insert.setColumns(columnList);
+
+        // 设置插入值
+        ExpressionList expressionList = (ExpressionList) insert.getItemsList();
+        expressionList.getExpressions().addAll(Arrays.asList(new StringValue("1"), new StringValue("2")));
+
+        System.err.println(insert); // INSERT INTO table (col01, col02) VALUES ('1', '2')
+    }
+
+    /**
+     * 构建更新语句
+     */
+    @Test
+    public void buildUpdateSql() {
+        // 创建表对象设置表名
+        Table table = new Table();
+        table.setName("table");
+
+        // 创建更新对象
+        Update update = new Update();
+        update.setTable(table);// 设置更新对象的表对象
+
+        // 设置更新列
+        List<Column> columnList = Arrays.asList(new Column("col01"), new Column("col02"));
+        update.setColumns(columnList);
+
+        // 设置更新值
+        update.setExpressions(Arrays.asList(new StringValue("1"), new StringValue("2")));
+
+        // 添加Where条件
+        EqualsTo equalsTo = new EqualsTo(); // 等于表达式
+        equalsTo.setLeftExpression(new Column(table, "user_id")); // 设置表达式左边值
+        equalsTo.setRightExpression(new StringValue("123456"));// 设置表达式右边值
+        update.setWhere(equalsTo); // 设置Where
+    }
+
+
+    /**
+     * 构建删除语句
+     */
+    public void buildDeleteSql() {
+        // 创建表对象设置表名
+        Table table = new Table();
+        table.setName("table");
+
+        // 创建更新对象
+        Delete delete = new Delete();
+        delete.setTable(table);// 设置更新对象的表对象
+
+        // 添加Where条件
+        EqualsTo equalsTo = new EqualsTo(); // 等于表达式
+        equalsTo.setLeftExpression(new Column(table, "user_id")); // 设置表达式左边值
+        equalsTo.setRightExpression(new StringValue("123456"));// 设置表达式右边值
+        delete.setWhere(equalsTo); // 设置Where
+
+        // 输入语句
+        System.err.println(delete);
+    }
 }
